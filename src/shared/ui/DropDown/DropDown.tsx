b2a2464938a2843758e5d429f67aa13875d348da
alexander.svirzhevsky <alexander.svirzhevsky@gmail.com>
2025-02-07 15:41:01 +0300

    added accessibility

--- src/shared/ui/DropDown/DropDown.tsx
+++ src/shared/ui/DropDown/DropDown.tsx
@@ -1,8 +1,16 @@
-import { ReactNode, useEffect, useLayoutEffect, useRef, useState } from 'react';
+import {
+  ReactNode,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+  KeyboardEvent,
+} from 'react';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import cn from './DropDown.module.scss';
 import { Link } from '../Link/Link';
 import { VStack } from '../Stack';
+import { useNavigate } from 'react-router-dom';
 
 interface MenuItem {
   title: string;
@@ -22,14 +30,72 @@ export const DropDown = ({
   items,
   className,
 }: DropDownProps) => {
+  const navigate = useNavigate();
   const menuRef = useRef<null | HTMLDivElement>(null);
   const buttonRef = useRef<null | HTMLDivElement>(null);
   const dropDownRef = useRef<null | HTMLDivElement>(null);
   const [isOpened, setIsOpened] = useState(false);
   const [dropDownPosition, setDropDownPosition] = useState({ right: 0 });
+  const [focusedIndex, setFocusedIndex] = useState<null | number>(null);
 
   const handleToggle = () => {
     setIsOpened((prev) => !prev);
+  };
+
+  const onKeyDownTriggerElement = (e: KeyboardEvent) => {
+    if (['ArrowDown', 'Enter'].includes(e.code)) {
+      handleToggle();
+      setFocusedIndex(0);
+    }
+
+    if (e.code === 'Escape') {
+      setIsOpened(false);
+      setFocusedIndex(null);
+    }
+  };
+
+  const onKeyDownItem = (e: KeyboardEvent, index: number) => {
+    console.log(e.code);
+
+    if (e.code === 'ArrowDown') {
+      setFocusedIndex((prev) => {
+        if (prev! < items.length - 1) {
+          return prev! + 1;
+        }
+
+        return 0;
+      });
+    }
+
+    if (e.code === 'ArrowUp') {
+      setFocusedIndex((prev) => {
+        if (prev! > 0) {
+          return prev! - 1;
+        }
+
+        return items.length - 1;
+      });
+    }
+
+    if (e.code === 'Escape') {
+      setIsOpened(false);
+      setFocusedIndex(null);
+      buttonRef.current?.focus();
+    }
+
+    if (e.code === 'Enter') {
+      const currentItem = items[index];
+
+      if (currentItem.url) {
+        navigate(currentItem.url);
+      } else if (currentItem.action) {
+        currentItem.action();
+      }
+
+      setIsOpened(false);
+      setFocusedIndex(null);
+      buttonRef.current?.focus();
+    }
   };
 
   useLayoutEffect(() => {
@@ -67,9 +133,22 @@ export const DropDown = ({
     };
   }, [isOpened]);
 
+  useEffect(() => {
+    if (isOpened && focusedIndex !== null) {
+      const element = document.getElementById(`dropdown-item-${focusedIndex}`);
+      element?.focus();
+    }
+  }, [isOpened, focusedIndex]);
+
   return (
     <div ref={menuRef} className={classNames(cn['DropDown'], {}, [className])}>
-      <div className={cn.triggerElement} ref={buttonRef} onClick={handleToggle}>
+      <div
+        className={cn.triggerElement}
+        ref={buttonRef}
+        onClick={handleToggle}
+        onKeyDown={onKeyDownTriggerElement}
+        tabIndex={0}
+      >
         {triggerElement}
       </div>
       {isOpened && (
@@ -82,12 +161,29 @@ export const DropDown = ({
         >
           <ul className={cn.menuList}>
             <VStack align="stretch">
-              {items.map(({ title, icon, url, action }) => (
-                <li key={title} className={cn.item}>
+              {items.map(({ title, icon, url, action }, index) => (
+                <li
+                  tabIndex={focusedIndex === index ? 0 : -1}
+                  key={title}
+                  className={classNames(
+                    cn.item,
+                    {
+                      [cn.focused]: index === focusedIndex,
+                    },
+                    [],
+                  )}
+                  id={`dropdown-item-${index}`}
+                  onKeyDown={(e) => onKeyDownItem(e, index)}
+                >
                   {icon && <span>{icon}</span>}
                   {url ? (
-                    <Link type="clear" to={url} onClick={handleToggle}>
-                      <div>{title}</div>
+                    <Link
+                      type="clear"
+                      to={url}
+                      tabIndex={-1}
+                      onClick={handleToggle}
+                    >
+                      {title}
                     </Link>
                   ) : (
                     <div
